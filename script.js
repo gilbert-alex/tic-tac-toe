65815a43706ec5d037934bfe0ddf71621e512f78
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-07-25 15:17:22 -0700

    Add end game conditions

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-const tictaktoe = (function () {
+const TicTakToe = (function () {
 
     function GameBoard() {
+        /* 3x3 grid
+            An array of three member arrays of size 3
+            Each child array is a space to hold player information
+        */
         const size = 3;
         const board = [];
     
@@ -18,9 +22,10 @@ const tictaktoe = (function () {
         const fillSpace = (symbol, row, col) => {
             if (board[row][col].getValue() === null) {
                 board[row][col].addSymbol(symbol);
+                return true;
             } else {
-                console.log(`row:${row} col:${col} is an invalid move`)
-                return
+                console.log(`Invalid Move: Row:${row} Col:${col} is already taken.`)
+                return false;
             }
             
         }
@@ -31,10 +36,12 @@ const tictaktoe = (function () {
             return values;
         }
     
+        // exposed to the IIFE
         return {getBoard, getSpace, fillSpace, getValues};
     }
 
     function BoardSpace() {
+        // Object to fill each space of the 3x3 grid
         let value = null;
     
         const addSymbol = (symbol) => {
@@ -42,6 +49,7 @@ const tictaktoe = (function () {
         }
         const getValue = () => value;
     
+        // exposed to the IIFE
         return {addSymbol, getValue};
     }
 
@@ -49,7 +57,9 @@ const tictaktoe = (function () {
         playerOneName = 'Player One',
         playerTwoName = 'Player Two'
     ) {
+        // private variables
         const board = GameBoard();
+        let gameOver = false;
     
         const players = [
             {
@@ -61,10 +71,9 @@ const tictaktoe = (function () {
                 symbol: 'O'
             }
         ];
-    
         let currentPlayer = players[0];
     
-        // public methods
+        // methods
         const getBoard = () => {
             return board.getBoard();
         }
@@ -72,35 +81,91 @@ const tictaktoe = (function () {
             console.log(board.getValues());
         }
         const makeMove = (row, col) => {
-            board.fillSpace(currentPlayer.symbol, row, col);
-            console.log(`${currentPlayer.name} puts a ${currentPlayer.symbol} at ${row}:${col}`);
+            if (board.fillSpace(currentPlayer.symbol, row, col)) {
+                return true;
+            } else {
+                return false;
+            };
+            // console.log(`${currentPlayer.name} puts a ${currentPlayer.symbol} at ${row}:${col}`);
         }
         const switchPlayer = () => {
             currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0];
-            console.log('players switched');
+            // console.log('players switched');
         }
         const checkWinner = (row, col) => {
-            let activeRow = board.getBoard()[row].map((space) => space.getValue());
-            let matchedRow = activeRow.filter((space) => space === currentPlayer.symbol);
-            let activeCol = board.getBoard().map((row) => row[col].getValue());
-            let matchedCol = activeCol.filter((space) => space === currentPlayer.symbol);
+            // check win along row
+            let matchedRow = board.getValues()[row].filter((space) => space === currentPlayer.symbol);
+            console.log(`row: ${matchedRow}`);
 
-            if (matchedRow.length === 3 || matchedCol.length === 3) {
+            // check win along column
+            // let activeCol = board.getBoard().map((row) => row[col].getValue());
+            // let matchedCol = activeCol.filter((space) => space === currentPlayer.symbol);
+            let matchedCol = board.getValues().map((row) => row[col]).filter((space) => space === currentPlayer.symbol);
+            console.log(`col: ${matchedCol}`);
+
+            // check win along diagonal top left bottom right (tlbr)
+            let tlbr = [];
+            for (let i = 0; i < board.getBoard().length; i++) {
+                tlbr.push(board.getBoard()[i][i].getValue());
+            }
+            let matchedTlbr = tlbr.filter((space) => space === currentPlayer.symbol);
+            console.log(`d1: ${matchedTlbr}`);
+
+            // check win along diagonal bottom left top right (bltr)
+            let bltr = [];
+            for (let j = 0; j < board.getBoard().length; j++){
+                bltr.push(board.getValues()[j].at(-(j+1)));
+            }
+            let matchedBltr = bltr.filter((space) => space === currentPlayer.symbol);
+            console.log(`d2: ${matchedBltr}`);
+
+            if (matchedRow.length === 3 ||
+                matchedCol.length === 3 ||
+                matchedTlbr.length === 3 ||
+                matchedBltr.length === 3
+            ) {
+                gameOver = true;
+            }
+        }
+        const checkTie = () => {
+            let emptySpaces = 0
+            board.getValues().map((row) => {row.map((space) => {
+                if (space === null) {
+                    emptySpaces++;
+                }
+            })})
+            if (emptySpaces === 0) {
+                gameOver = true
+            };
+            console.log(`there are ${emptySpaces} empty spaces left`);
+        }
+        const endGame = () => {
+            if (gameOver) {
                 console.log('game over');
             }
-            console.log(matchedRow);
-            console.log(matchedCol);
         }
         const playerTurn = (row, col) => {
-            makeMove(row, col);
-            checkWinner(row, col);
-            viewBoard();
-            switchPlayer();
+            if (!gameOver) {
+                if (makeMove(row, col)) {
+                    checkWinner(row, col);
+                    checkTie();
+                    viewBoard();
+                    endGame();
+                    switchPlayer();
+                } else {
+                    console.log('try again');
+                };
+
+            } else {
+                console.log('game is already over');
+            }
         }
 
+        // public methods
         return {getBoard, playerTurn};
     }
 
+    // creating method
     return {GameController};
 })();
 
@@ -108,6 +173,48 @@ const tictaktoe = (function () {
 
 
 // test
-const game1 = tictaktoe.GameController('Alex', 'Lauren');
+const game1 = TicTakToe.GameController();
 
-game1.playerTurn(0,0);
+/* test invalid move and auto player switch*/
+// game1.playerTurn(0,0);
+// game1.playerTurn(0,0);
+// game1.playerTurn(1,1);
+
+/* test row end game condition */
+// game1.playerTurn(0,0);
+// game1.playerTurn(1,0);
+// game1.playerTurn(0,1);
+// game1.playerTurn(1,1);
+// game1.playerTurn(0,2);
+
+/* test column end game condition */
+// game1.playerTurn(0,0);
+// game1.playerTurn(0,1);
+// game1.playerTurn(1,0);
+// game1.playerTurn(1,1);
+// game1.playerTurn(2,0);
+
+/* test tlbr end game condition */
+// game1.playerTurn(0,0);
+// game1.playerTurn(1,0);
+// game1.playerTurn(1,1);
+// game1.playerTurn(0,2);
+// game1.playerTurn(2,2);
+
+/* test bltr end game condition */
+// game1.playerTurn(2,0);
+// game1.playerTurn(0,1);
+// game1.playerTurn(1,1);
+// game1.playerTurn(2,1);
+// game1.playerTurn(0,2);
+
+/* test tie end game condition */
+// game1.playerTurn(0,0);
+// game1.playerTurn(0,1);
+// game1.playerTurn(1,0);
+// game1.playerTurn(1,1);
+// game1.playerTurn(2,1);
+// game1.playerTurn(2,0);
+// game1.playerTurn(0,2);
+// game1.playerTurn(1,2);
+// game1.playerTurn(2,2);
